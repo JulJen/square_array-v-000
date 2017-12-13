f97d69eef58a9e31a2db02bd91d8878d78570960
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-13 21:41:19 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,5 +3,5 @@ def square_array(array)
   array.each do |squared| #each is called on collection of array and passed through block of squared
   new_array << squared ** 2 #block to the exponent of 2 is added to the end of a new array
   end
-  new_array
+  new_array #returns array with new values
 end
