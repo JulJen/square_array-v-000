3169095e9e0c2a3a934879657fcbe25ba069aef6
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-13 21:26:34 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,13 @@
 def square_array(array)
-  array = [1,2,3]
-  counter = 1
+  square_array = []
   array.each do |array|
-    puts 
+    puts
   count +=1
 end
 
 
 # describe '#square_array' do
-# 
+#
 #   it 'does not call on collect/map or inject' do
 #     numbers = [1,2,3]
 #     expect(numbers).to_not receive(:collect)
@@ -16,16 +15,16 @@ end
 #     expect(numbers).to_not receive(:inject)
 #     square_array(numbers)
 #   end
-# 
+#
 #   it 'calls on each' do
 #     numbers = [1,2,3]
 #     expect(numbers).to receive(:each)
 #     square_array(numbers)
 #   end
-# 
+#
 #   it 'should square the elements in an array' do
 #     expect(square_array([1,2,3])).to eq([1,4,9])
 #     expect(square_array([9,10,16,25])).to eq([81,100,256,625])
 #   end
-# 
+#
 # end
