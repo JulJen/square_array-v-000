3830be962cb2f0ee806c9d61c41429d2576977f7
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-13 21:40:30 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,15 +1,15 @@
+# def square_array(array)
+#   new_array = Array.new  #creating an empty array using a class constructor
+#   array.each do |squared| #each is called on collection of array and passed through block of squared
+#   new_array << squared ** 2 #block to the exponent of 2 is added to the end of a new array
+#   end
+#   new_array
+# end
+#
+
 def square_array(array)
   new_array = Array.new  #creating an empty array using a class constructor
-  array.each do |squared| #each is called on collection of array and passed through block of squared
-  new_array << squared ** 2 #block to the exponent of 2 is added to the end of a new array
-  end
-  new_array
-end
-
-
-def square_array(array)
-  new_array = Array.new  #creating an empty array using a class constructor
-  array.collect {|squared|squared **2} #each is called on collection of 
+  array.collect {|squared|squared **2} #each is called on collection of
   end
   new_array
 end
